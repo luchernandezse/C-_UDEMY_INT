8b746fe8a7076ddd6940542bff81f4c697817c8b
luchernandezse <luchernandezse@gmail.com>
2024-01-04 16:23:32 -0500

    Sesion4

---
+++ Stack/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.CodeDom;
+
+partial class Program
+{
+    static void Main()
+    {
+        // Create object _list
+        Stack _list = new Stack();
+
+        // Push elements
+        _list.Push(1);
+        _list.Clear();
+        _list.Push("bull");
+        _list.Push(3);
+
+
+        // Pop elements from the stack
+
+        Console.WriteLine(_list.Pop());
+        Console.WriteLine(_list.Pop());
+        Console.WriteLine(_list.Pop());
+        Console.WriteLine(_list.Pop());
+        
+
+
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+partial class Program
+{
+    public class Stack
+    {
+        public List<object> _list = new List<object>();
+
+        public Stack()
+        {
+
+        }
+        public void Push(object num)
+        {
+            if (num == null)
+            {
+                throw new InvalidOperationException("Cannot add null object");
+            }
+            _list.Insert(0,num);
+        }
+        public object Pop()
+        {
+            int elem = _list.Count;
+            if (elem < 1)
+            {
+                throw new InvalidOperationException("No elements to show");
+
+            }
+            object a= _list[0];
+            _list.RemoveAt(0);
+            return a;
+            
+            
+        }
+        public void Clear()
+        {
+            _list.Clear();
+        }
+    }
+}
